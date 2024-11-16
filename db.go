335584f3cbead7975a82193bb9d8861665e43084
Sanjar <sanjarubuntu@gmail.com>
2024-11-16 14:27:53 +0500

    db.go and orm.go created and updated

---
+++ db.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+    dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5433 sslmode=disable"
+    var err error
+
+    // Подключение к базе данных
+    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    // Проверка на ошибки подключения
+    if err != nil {
+        log.Fatal("Не удалось подключиться к базе данных", err)
+    }
+}
