9ac61f6826b5a70f961923046b86f2342da9d682
Sanjar <sanjarubuntu@gmail.com>
2024-11-16 16:42:02 +0500

    Updated Create and GetMessages functions

--- main.go
+++ main.go
@@ -39,11 +39,36 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 
 func CreateMessage (w http.ResponseWriter, r *http.Request) {
 	// Обновить POST ручку, чтобы она записывала содержимое task в БД (Передаем джейсон с полями task и is_done)
+	var data Message
+    // Декодирование JSON из запроса в структуру
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+		return
+	}
 
+    // Сохранение в базе данных
+	if err := DB.Create(&data).Error; err != nil{
+		http.Error(w, "Failed to create message", http.StatusInternalServerError)
+		return
+	}
+
+    // Ответ с подтверждением
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Message created successfully"})
 }
 
 func GetMessages (w http.ResponseWriter, r *http.Request) {
 	// Обновить GET ручку, чтобы она выводила слайс task (все message, которые лежат в БД)
+	var messages []Message
+
+    // Получаем все сообщения из базы данных
+	if err := DB.Find(&messages).Error; err != nil {
+		http.Error(w, "Failed to retrieve messages", http.StatusInternalServerError)
+		return
+	}
+    // Отправляем все сообщения в ответе
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(messages) 
 }
 
 func main() {
