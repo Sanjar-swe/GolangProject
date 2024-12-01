4b35087dab0207a09d20e829b937691ffe4a0ae0
Sanjar <sanjarubuntu@gmail.com>
2024-12-01 12:54:07 +0500

    Updated openapi.yaml

--- internal/handlers/taskHandlers.go
+++ internal/handlers/taskHandlers.go
@@ -2,14 +2,9 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
-	"net/http"
-	"strconv"
 
-	"github.com/Sanjar-swe/GolangProject/internal/database"
 	"github.com/Sanjar-swe/GolangProject/internal/taskService"
 	"github.com/Sanjar-swe/GolangProject/internal/web/tasks"
-	"github.com/gorilla/mux"
 )
 
 type Handler struct {
@@ -95,41 +90,41 @@ func NewHandler(service *taskService.TaskService) *Handler {
 // 	json.NewEncoder(w).Encode(createdTask)
 // }
 
-func (h *Handler) PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Invalid task ID", http.StatusBadRequest)
-		return
-	}
-	var task taskService.Message
-	if err := database.DB.First(&task, id).Error; err != nil {
-		http.Error(w, "Task not found", http.StatusNotFound)
-		return
-	}
-	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
-		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
-		return
-	}
-	updatedTask, error := h.Service.UpdateTaskByID(uint(id), task)
-	if error != nil {
-		http.Error(w, error.Error(), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(updatedTask)
-}
+// func (h *Handler) PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	id, err := strconv.Atoi(vars["id"])
+// 	if err != nil {
+// 		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+// 		return
+// 	}
+// 	var task taskService.Message
+// 	if err := database.DB.First(&task, id).Error; err != nil {
+// 		http.Error(w, "Task not found", http.StatusNotFound)
+// 		return
+// 	}
+// 	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+// 		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+// 		return
+// 	}
+// 	updatedTask, error := h.Service.UpdateTaskByID(uint(id), task)
+// 	if error != nil {
+// 		http.Error(w, error.Error(), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(updatedTask)
+// }
 
-func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Invalid task ID", http.StatusBadRequest)
-		return
-	}
-	error := h.Service.DeleteTaskByID(uint(id))
-	if error != nil {
-		http.Error(w, error.Error(), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "Task deleted successfully"})
-}
+// func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r)
+// 	id, err := strconv.Atoi(vars["id"])
+// 	if err != nil {
+// 		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+// 		return
+// 	}
+// 	error := h.Service.DeleteTaskByID(uint(id))
+// 	if error != nil {
+// 		http.Error(w, error.Error(), http.StatusInternalServerError)
+// 	}
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(map[string]string{"message": "Task deleted successfully"})
+// }
